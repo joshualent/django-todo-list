9fa1dfa157703c1dae09e6fdac446ce6910f06aa
Joshua Lent <Joshualent83@gmail.com>
2025-03-20 12:06:18 -0400

    Add temp home page

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),  # new
     # Add app urls below (ie. "pages.urls")
     # Remove before use
-    path("about/", TemplateView.as_view(template_name="about.html")),
+    path("about/", TemplateView.as_view(template_name="about.html"), name="home"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
